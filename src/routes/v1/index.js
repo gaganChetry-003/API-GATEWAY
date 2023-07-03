8bb04152e284a5f7aee2d64aba9f5074faae379c
gagan_003 <gaganchetry0702@gmail.com>
2023-07-03 14:43:27 +0530

    Added encryption in user signup

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const {InfoController}=require('../../controllers');
+const userRouter = require('./user-routes');
 const router=express.Router();
 router.get('/info',InfoController.info);
+router.use('/signup', userRouter);
 module.exports=router;
